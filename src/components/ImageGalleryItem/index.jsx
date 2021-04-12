40ed4fc1a86406bd595d4783f533d0615fe25a7e
Artyom Kupits <kup1910@gmail.com>
2021-04-12 20:55:12 +0300

    2

--- src/components/ImageGalleryItem/index.jsx
+++ src/components/ImageGalleryItem/index.jsx
@@ -1,14 +1,1 @@
-import styles from './ImageGalleryItem.module.scss';
-
-const ImageGalleryItem = ({ url, alt, onToggle, onLargeImage }) => {
-  return (
-    <li
-      className={styles.ImageGalleryItem}
-      onClick={() => onToggle(onLargeImage, alt)}
-    >
-      <img src={url} alt={alt} className={styles.ImageGalleryItem__image} />
-    </li>
-  );
-};
-
-export default ImageGalleryItem;
+export { default } from './ImageGalleryItem';
